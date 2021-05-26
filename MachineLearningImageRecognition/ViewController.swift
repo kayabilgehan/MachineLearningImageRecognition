dc3746be01bc96c6895ff34dcf9f0d5b581db65e
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-05-26 03:08:06 +0300

    App Finished

--- MachineLearningImageRecognition/ViewController.swift
+++ MachineLearningImageRecognition/ViewController.swift
@@ -1,19 +1,73 @@
-//
-//  ViewController.swift
-//  MachineLearningImageRecognition
-//
-//  Created by Bilgehan KAYA on 25.05.2021.
-//
-
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultLabel.text = "";
     }
-
-
+    
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
+        if let ciImage = CIImage(image: imageView.image!) {
+            recognizeImage(image: ciImage)
+        }
+    }
+    func recognizeImage(image: CIImage){
+        //1. Request
+        //2. Handler
+        
+        resultLabel.text = "Searching.."
+        
+        do{
+            //REQUEST
+            if let model = try? VNCoreMLModel(for: MobileNetV2().model) {
+                let request = VNCoreMLRequest(model: model) { vnrequest, error in
+                    if error != nil {
+                        print("Error!")
+                    } else {
+                        if let results = vnrequest.results as? [VNClassificationObservation] {
+                            if results.count > 0 {
+                                let topResult = results.first
+                                
+                                DispatchQueue.main.async {
+                                    let confidanceLevel = (topResult?.confidence ?? 0) * 100
+                                    let rounded = Int(confidanceLevel * 100) / 100
+                                    self.resultLabel.text = "\(rounded)% \(topResult!.identifier)"
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                let handler = VNImageRequestHandler(ciImage: image)
+                DispatchQueue.global(qos: .userInteractive).async {
+                    do {
+                        try handler.perform([request])
+                    }
+                    catch {
+                        print("Error!")
+                    }
+                }
+            }
+        } catch {
+            print("Error!")
+        }
+        
+        
+    }
 }
 
